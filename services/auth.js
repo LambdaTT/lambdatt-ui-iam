2e68e2d18f6f75ac87da99f4d698f5eab0c6d100
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2024-12-19 19:05:07 -0300

    added function logout on auth service

--- services/auth.js
+++ services/auth.js
@@ -17,5 +17,25 @@ export default {
           location.href = `/login?goTo=${$component.$route.path}`;
         }
       });
+  },
+
+  logout($component) {
+    $component.$emit('load', 'logout');
+
+    var url = '/api/iam/auth/v1/logout';
+
+    if (localStorage.getItem('authtoken'))
+      url += '?token=' + localStorage.getItem('authtoken');
+
+    return http.delete(url)
+      .then(function () {
+        $component.$emit('loaded', 'logout');
+        localStorage.removeItem('authtoken');
+        localStorage.removeItem('xsrf_token');
+        localStorage.removeItem('iam_session_key');
+        localStorage.removeItem('regularPermissions');
+        localStorage.removeItem('customPermissions');
+        location.href = '/login';
+      });
   }
 }
