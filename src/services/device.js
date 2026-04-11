87855b8d04522abcbdded1032a6bbcd240eebf3d
Gabriel Valentoni <gabriel@lambdatt.com.br>
2026-04-11 19:14:43 -0300

    refactor: standardize code style and formatting in device service

--- src/services/device.js
+++ src/services/device.js
@@ -1,32 +1,27 @@
-import $sys from "src/lambdatt.js";
-import ENDPI from "../ENDPOINTS";
+import $sys from 'src/lambdatt.js'
+import ENDPOINTS from '../ENDPOINTS'
 
 export default {
   async registerDevice() {
     try {
-      const { data } = await $sys
-        .getService("toolcase/http")
-        .post(ENDPOINTS.DEVICES.DEVICE);
+      const { data } = await $sys.getService('toolcase/http').post(ENDPOINTS.DEVICES.DEVICE)
 
-      localStorage.setItem("iam_device_key", data.ds_key);
+      localStorage.setItem('iam_device_key', data.ds_key)
     } catch (error) {
-      console.error(
-        "An error occurred while attempting to register device.",
-        error,
-      );
+      console.error('An error occurred while attempting to register device.', error)
     }
 
-    return;
+    return
   },
 
   async getDeviceKey() {
     // Handle device key
-    var deviceKey = localStorage.getItem("iam_device_key");
+    var deviceKey = localStorage.getItem('iam_device_key')
     if (!deviceKey) {
-      await this.registerDevice();
-      deviceKey = localStorage.getItem("iam_device_key");
+      await this.registerDevice()
+      deviceKey = localStorage.getItem('iam_device_key')
     }
 
-    return deviceKey;
+    return deviceKey
   },
-};
+}
